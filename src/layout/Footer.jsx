4140116246b0540ce3bd3a2b48c7dc0a5473ffb0
krepillah <saprykin.vladislav@gmail.com>
2024-05-30 19:35:27 +0300

    Header Body Footer
    The begining of project

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,30 @@
+function Footer() {
+    return (
+        <footer className="page-footer indigo">
+            <div className="container">
+            <div className="row">
+                <div className="col l6 s12">
+                <h5 className="white-text">Полезное</h5>
+                <p className="grey-text text-lighten-4">Представьте, что здесь классный футер.</p>
+                </div>
+                <div className="col l4 offset-l2 s12">
+                <h5 className="white-text">Ссылки на соцсети</h5>
+                <ul>
+                    <li><a className="grey-text text-lighten-3" href="#!">Инстаграм</a></li>
+                    <li><a className="grey-text text-lighten-3" href="#!">Гитхаб</a></li>
+                    <li><a className="grey-text text-lighten-3" href="#!">Телеграм</a></li>
+                </ul>
+                </div>
+            </div>
+            </div>
+            <div className="footer-copyright indigo darken-1">
+            <div className="container">
+            © {new Date().getFullYear()} Copyright Text
+            <a className="grey-text text-lighten-4 right" href="#!">Больше о нас</a>
+            </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/layout/Main.jsx
@@ -0,0 +1,9 @@
+function Main() {
+    return (
+        <main className="container content">
+            Movies app body
+        </main>
+    )
+}
+
+export default Main;
